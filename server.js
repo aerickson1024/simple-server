7b4ce0da18bdf067695fc7b2cf06d17edb76bb27
Andy Erickson <andy.w.erickson@gmail.com>
2021-02-12 15:18:41 -0600

    added index

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+
 const PORT = process.env.PORT || 3000;
 
+app.use(express.static(__dirname));
 app.get('/', (req, res) => {
-  res.send({ message: "hello world"});
+  res.sendFile(path.resolve(__dirname, 'index.html'));
 });
 
 app.listen(PORT, () => {
